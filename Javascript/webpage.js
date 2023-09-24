1e6a0465f37496cdaeb25ce87d65650770c4efdd
svelasco14 <svheatfan3@gmail.com>
2023-09-23 23:16:36 -0400

    2.0

---
+++ Javascript/webpage.js
@@ -0,0 +1,58 @@
+// responsive hamburger
+addResp.addEventListener("click", () => {
+  let myResp = document.getElementById("myNav");
+  
+  if (myResp.className === "nav") {
+    myResp.className += " responsive";
+  } else {
+    myResp.className = "nav";
+  }
+});
+
+// image slider
+const images = document.querySelectorAll('#slider img');
+const previousImage = document.getElementById("prev");
+const nextImage = document.getElementById("next");
+
+let currentIndex = 0;
+
+function reset() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.remove('active1');
+  }
+}
+
+function initializeSlider() {
+  reset();
+  images[currentIndex].classList.add('active1');
+}
+
+function slideLeft() {
+  reset();
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  images[currentIndex].classList.add('active1');
+}
+
+function slideRight() {
+  reset();
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  images[currentIndex].classList.add('active1');
+}
+
+initializeSlider();
+
+previousImage.addEventListener('click', function() {
+  slideLeft();
+});
+
+nextImage.addEventListener('click', function() {
+  slideRight();
+});
+
+
